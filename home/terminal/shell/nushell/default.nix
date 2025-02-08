a7aba2002cd96e018445492a5045149aaa674667
Brayan-724 <bjqf0724@gmail.com>
2025-02-07 20:12:46 -0700

    chore(nushell): Use `nom` instead of `nix`

--- home/terminal/shell/nushell/default.nix
+++ home/terminal/shell/nushell/default.nix
@@ -8,7 +8,7 @@
       ns = "${pkgs.nh}/bin/nh os switch --ask -H asce -- --show-trace";
       glol = "${pkgs.git}/bin/git log --graph --all --decorate --format=format:'%C(dim)%h - %C(reset)%C(bold cyan)%ah %C(green)(%ar)%C(yellow)%d%C(reset)%n          %s%C(dim
 white) - %an%C(reset)'";
-      nix-zellij = "nix develop --impure -c zellij attach --create";
+      nix-zellij = "nom develop --impure -c zellij attach --create";
     };
 
     environmentVariables = {
