37bb9aa730b42de2b929150d8580fffc70e96414
Brayan-724 <bjqf0724@gmail.com>
2025-06-21 02:29:38 -0700

    fix(modules): nh declaration

--- amix-modules/nh.nix
+++ amix-modules/nh.nix
@@ -12,22 +12,20 @@
     path = amixTypes.string;
   };
 
-  config = {
-    amix.shell.aliases = {
-      ns = "${amixLib.getExe config.nh.pkg} os switch --ask -H ${profileName} -- --show-trace";
-    };
+  amix.aliases = {
+    ns = "${amixLib.getExe config.nh.pkg} os switch --ask -H ${profileName} -- --show-trace";
+  };
 
-    # nh default flake
-    environment.variables.FLAKE = config.nh.path;
+  # nh default flake
+  nixos.environment.variables.FLAKE = config.nh.path;
 
-    programs.nh = {
+  nixos.programs.nh = {
+    enable = true;
+
+    # weekly cleanup
+    clean = {
       enable = true;
-
-      # weekly cleanup
-      clean = {
-        enable = true;
-        extraArgs = "--keep-since 15d";
-      };
+      extraArgs = "--keep-since 15d";
     };
   };
 }
